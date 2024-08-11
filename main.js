d9accb3a49b60bc2e542aac9741c7e0d4997dac4
Helal <106902696+helalsheikh@users.noreply.github.com>
2024-08-11 12:14:12 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let menu = document.getElementById("nav");
+let open = document.getElementById("menu-btn");
+let close =document.getElementById("close");
+
+
+
+function openmunu(){
+    menu.style.left="0";
+    open.style.display="none";
+    close.style.display="block";
+}
+
